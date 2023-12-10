77126f79c2969155f1b80e4b85baaed74ce5201a
zanciks <zackarysemancik@gmail.com>
2023-12-10 11:41:09 -0700

    Freestanding binary. Doesn't do anything else yet

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// disable the standard library, as it is OS-based
+#![no_std]
+// we cant have a main function. instead, we define _start()
+#![no_main]
+// we need to be able to deal with panics
+use core::panic::PanicInfo;
+
+// by using no_mangle, the function will actually be named _start() when compiled
+// (otherwise, it would be named something random/mangled)
+// this is needed, as machine code uses _start to begin its code
+// pub extern C just means that we use the C naming convention, instead of Rust's.
+// we should never return from this function, so we a "returning" the never type
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+// again, we return the never type, as when we panic, we don't return.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}

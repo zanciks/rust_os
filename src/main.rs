5d18e0a205d5966822464588d60a897069dc4df5
zanciks <zackarysemancik@gmail.com>
2023-12-10 14:17:47 -0700

    added complicated tests for boot, should panic, and vga_buffer

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@
 #![no_main]
 // custom tests
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-// we need to be able to deal with panics
-use core::panic::PanicInfo;
-use x86_64;
 
-mod vga_buffer;
-mod serial;
+
+use rust_os::println;
+use core::panic::PanicInfo;
 
 // by using no_mangle, the function will actually be named _start() when compiled
 // (otherwise, it would be named something random/mangled)
@@ -45,55 +43,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    // exit qemu
-    exit_qemu(QemuExitCode::Success);
+    rust_os::test_panic_handler(info)
 }
 
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-// we number the enums because we should define our own error codes
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-    where
-        T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
 }
